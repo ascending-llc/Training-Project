76663e3cd82267cecab7570b1c759b7b428d976d
ryo <ryo.hang@gmail.com>
2020-08-01 10:31:38 -0400

    add profile

--- src/main/java/com/ascending/training/config/AWSConfig.java
+++ src/main/java/com/ascending/training/config/AWSConfig.java
@@ -1,5 +1,6 @@
 package com.ascending.training.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@Profile("dev")
+@Profile({"dev","prod"})
 public class AWSConfig {
     //    @Bean
 //    public DepartmentService getDepartmentService(){
@@ -35,7 +36,7 @@ public class AWSConfig {
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public AmazonS3 getAmazonS3() {
         return  AmazonS3ClientBuilder
-                .standard()
+                .standard().withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(Regions.US_EAST_1)
                 .build();
     }
